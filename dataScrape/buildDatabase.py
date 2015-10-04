854beda71c34acccc002aa8074e23a689dd2d74e
Brian <brichen95@gmail.com>
2015-10-04 05:32:02 -0400

    the FDA actually does not provide good information. at all.

--- dataScrape/buildDatabase.py
+++ dataScrape/buildDatabase.py
@@ -30,8 +30,10 @@ numDrugs = getNumOfDrugs()
 offset = 0
 numNamedDrugs = 0
 numRequests = 0
+drugsSaved = []
 
-while offset < numDrugs:
+#Thanks to the FDA, we actually can't poll more than 5000. -_-
+while offset < 5000:
     #launch request, convert json to dict
     r = requests.get((url + 'skip=' + str(offset)), data={'apikey': 'PbYvWZzzI8MkEZwGVYCdNXb8kZIp4Itcrp6hfZNJ'})
     data = json.loads(r.text)
@@ -41,29 +43,32 @@ while offset < numDrugs:
     for drug in data['results']:
         #If the drug has no name, we don't really care about it
         if 'brand_name' in drug['openfda'] or 'generic_name' in drug['openfda']:
-            #20 lashes for what I'm about to write...
-            drugInfo = {'brand_name': drug['openfda']['brand_name'] if 'brand_name' in drug['openfda'] else drug['openfda']['generic_name'],
-                          'generic_name': drug['openfda']['generic_name'] if 'generic_name' in drug['openfda'] else '',
-                          'do_not_use': drug['do_not_use'] if 'do_not_use' in drug else '',
-                          'stop_use': drug['stop_use'] if 'stop_use' in drug else '',
-                          'boxed_warning': drug['boxed_warning'] if 'boxed_warning' in drug else '',
-                          'warnings_and_precautions':  drug['warnings_and_precautions'] if 'warnings_and_precautions' in drug else '',
-                          'warnings': drug['warnings'] if 'warnings' in drug else '',
-                          'user_safety_warnings': drug['user_safety_warnings'] if 'user_safety_warnings' in drug else '',
-                          'precautions': drug['precautions'] if 'precautions' in drug else '',
-                          'general_precautions': drug['general_precautions'] if 'general_precautions' in drug else '',
-                          'use_in_specific_populations': drug['use_in_specific_populations'] if 'use_in_specific_populations' in drug else '',
-                          'package_label_principal_display_panel': drug['package_label_principal_display_panel'] if 'package_label_principal_display_panel' in drug else '',
-                          'information_for_patients': drug['information_for_patients'] if 'information_for_patients' in drug else '',
-                          'when_using': drug['when_using'] if 'when_using' in drug else '',
-                          'inactive_ingredient': drug['inactive_ingredient'] if 'inactive_ingredient' in drug else '',
-                          'active_ingredient': drug['active_ingredient'] if 'active_ingredient' in drug else '',
-                          'ask_doctor': drug['ask_doctor'] if 'ask_doctor' in drug else '',
-                          'ask_doctor_or_pharmacist': drug['ask_doctor_or_pharmacist'] if 'ask_doctor_or_pharmacist' in drug else '',
-                          'other_safety_information': drug['other_safety_information'] if 'other_safety_information' in drug else '',
-                      }
-            saveToDatabase(drugInfo)
-            numNamedDrugs += 1
+            name = drug['openfda']['brand_name'] if 'brand_name' in drug['openfda'] else drug['openfda']['generic_name']
+            if name not in drugsSaved:
+                #20 lashes for what I'm about to write...
+                drugInfo = {'brand_name': name,
+                              'generic_name': drug['openfda']['generic_name'] if 'generic_name' in drug['openfda'] else '',
+                              'do_not_use': drug['do_not_use'] if 'do_not_use' in drug else '',
+                              'stop_use': drug['stop_use'] if 'stop_use' in drug else '',
+                              'boxed_warning': drug['boxed_warning'] if 'boxed_warning' in drug else '',
+                              'warnings_and_precautions':  drug['warnings_and_precautions'] if 'warnings_and_precautions' in drug else '',
+                              'warnings': drug['warnings'] if 'warnings' in drug else '',
+                              'user_safety_warnings': drug['user_safety_warnings'] if 'user_safety_warnings' in drug else '',
+                              'precautions': drug['precautions'] if 'precautions' in drug else '',
+                              'general_precautions': drug['general_precautions'] if 'general_precautions' in drug else '',
+                              'use_in_specific_populations': drug['use_in_specific_populations'] if 'use_in_specific_populations' in drug else '',
+                              'package_label_principal_display_panel': drug['package_label_principal_display_panel'] if 'package_label_principal_display_panel' in drug else '',
+                              'information_for_patients': drug['information_for_patients'] if 'information_for_patients' in drug else '',
+                              'when_using': drug['when_using'] if 'when_using' in drug else '',
+                              'inactive_ingredient': drug['inactive_ingredient'] if 'inactive_ingredient' in drug else '',
+                              'active_ingredient': drug['active_ingredient'] if 'active_ingredient' in drug else '',
+                              'ask_doctor': drug['ask_doctor'] if 'ask_doctor' in drug else '',
+                              'ask_doctor_or_pharmacist': drug['ask_doctor_or_pharmacist'] if 'ask_doctor_or_pharmacist' in drug else '',
+                              'other_safety_information': drug['other_safety_information'] if 'other_safety_information' in drug else '',
+                          }
+                saveToDatabase(drugInfo)
+                numNamedDrugs += 1
+                drugsSaved.append(name)
 
     #set offset
     offset += 100
