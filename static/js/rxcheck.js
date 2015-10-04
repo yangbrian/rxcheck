8fe4cb1ab0f6b9173ca093f4387bbf6aeec6cd77
Brian <brichen95@gmail.com>
2015-10-04 04:41:08 -0400

    warnings work!

--- static/js/rxcheck.js
+++ static/js/rxcheck.js
@@ -1,5 +1,5 @@
 var drugNames = ['CHANTECAILLE', 'Ipratropium Bromide', 'ECZEMA REAL RELIEF', 'Amnesteem'];
-var conditions = ['pregnant', 'breast-feeding'];
+var conditions = ['pregnant', 'breast-feeding', 'psychosis'];
 
 $(document).ready(function() {
     $('#drugs-input').on('keyup', function(e) {
@@ -52,10 +52,21 @@ function addDrug(value, suggestions) {
 
 $(document).ready(function() {
     $('#warningsButton').on('click', function(e) {
+        //clear table first
+        $('#resultTable tr').has('td').remove();
+
         $.each(drugNames, function(index, value) {
             $.getJSON('/get/warnings/' + encodeURIComponent(value), function(data) {
-                var warnings = data.warnings_and_precautions.length > data.warnings.length ? data.warnings_and_precautions : data.warnings;
+                var warnings = String(data.warnings_and_precautions.length > data.warnings.length ? data.warnings_and_precautions : data.warnings);
                 var row = $('<tr>');
+
+                //Parse through text. If any of the conditions are said, bold it.
+                $.each(conditions, function(index, value) {
+                    if (warnings.indexOf(value) != -1)
+                        row.addClass('warning');
+                    var re = new RegExp(value, "gi");
+                    warnings = warnings.replace(re, ('<strong>' + value + '</strong>'));
+                });
 
                 row.append('<td>' + data.brand_name + '</td>');
                 row.append('<td>' + data.generic_name + '</td>');
