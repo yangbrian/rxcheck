bc04155e39d8e8d90dae18b5110a0ba81bcfd567
Brian <brichen95@gmail.com>
2015-10-04 09:48:00 -0400

    lol wrong quotations

--- dataScrape/getConditions.py
+++ dataScrape/getConditions.py
@@ -1,33 +1,22 @@
 __author__ = 'Brian'
 
 from bs4 import BeautifulSoup
-import requests, csv
+from string import ascii_lowercase
+import requests
 
-def saveToDatabase(data):
-    #Connect to mongodb client
-    client = MongoClient('localhost', 27017)
+url = 'https://rarediseases.info.nih.gov/gard/browse-by-first-letter/'
 
-    #Get the database
-    db = client.rxcheck
+for c in ascii_lowercase:
+    r = requests.get(url + c)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    diseaseList = []
+    superstring = ''
 
-    #Get the collection
-    collection = db.conditions
+    for listItem in soup.find_all("td", class_="DiseaseList"):
+        string = listItem.text
+        string = '\"' + string[:string.find(' - ')].strip() + '\", '
+        diseaseList.append(string)
+        superstring += string
 
-    #Save a new document into the collection
-    collection.insert_one(data)
-
-url = 'https://rarediseases.info.nih.gov/gard/browse-by-first-letter/Z'
-
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-diseaseList = []
-superstring = ''
-
-for listItem in soup.find_all("td", class_="DiseaseList"):
-    string = listItem.text
-    string = '\'' + string[:string.find(' - ')].strip() + '\', '
-    diseaseList.append(string)
-    superstring += string
-
-with open('test', 'a') as myfile:
-    myfile.write(superstring)
+    with open('test', 'a') as myfile:
+        myfile.write(superstring)
